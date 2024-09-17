6519e26c0a1045420119a569994ccdeaf0b0f028
Trung Kien <hoangtrungkien2109@gmail.com>
2024-09-17 19:45:24 +0700

    Update: Edit retrieve value box function

--- BillReader/search_using_yolo.py
+++ BillReader/search_using_yolo.py
@@ -1,6 +1,17 @@
 from ultralytics import YOLO
 import cv2
 
-if __name__ == '__main__':
-    model = YOLO("runs/detect/train9/weights/best.pt")
-    results = model.predict(source="data/images", save=True, save_txt=True)
+
+def find_field_yolo(model_path, src_path):
+    model = YOLO(model_path)
+    results = model.predict(source=src_path, save=True, save_txt=True)
+    return results
+
+
+def train_yolo(yaml_path, runs_path, epochs, pretrained=None):
+    if pretrained:
+        model = YOLO(pretrained)
+    model.train(data=yaml_path, epochs=epochs, resume=True if pretrained else False, project=runs_path)
+
+if __name__ == "__main__":
+    print(find_field_yolo("runs/detect/train9_50e_300train/weights/best.pt", "data/images"))
