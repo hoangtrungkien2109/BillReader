fa135c94d922b8636be4eba77aaea97104b2a53d
hoangtrungkien2109 <hoangtrungkien2109@gmail.com>
2024-09-18 19:57:49 +0700

    Delete: Delete error files

--- BillReader/BillReader/search_using_yolo.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-if __name__ == '__main__':
-    model = YOLO("runs/detect/train9/weights/best.pt")
-    results = model.predict(source="data/images", save=True, save_txt=True)
