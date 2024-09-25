f3447f4741fe2c26a6459296b6f7e2d489fb0637
Chau <tnchau23@gmail.com>
2024-09-25 19:11:51 +0700

    Update MongoDB

--- Flask/app.py
+++ Flask/app.py
@@ -1,41 +1,40 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash, send_file, jsonify
-import os,cv2
+import os, cv2
 from datetime import timedelta
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 from werkzeug.utils import secure_filename
-app= Flask(__name__)
+
+app = Flask(__name__)
 app._static_folder = ''
-app.config['UPLOAD_FOLDER'] = 'Flask/image_user'
+app.config['UPLOAD_FOLDER'] = 'Image'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-app.secret_key="123"
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///users.sqlite3'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
-app.permanent_session_lifetime= timedelta(minutes=5)
+app.secret_key = "123"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
-db =SQLAlchemy(app)
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False) 
-    password = db.Column(db.String(80), nullable=False)
-    images = db.relationship('Image', backref='user', lazy=True)
+# Thiết lập kết nối MongoDB
+# client = MongoClient('mongodb://localhost:27017/')
+uri = "mongodb+srv://tnchau23823:abc13579@cluster0.fs6jd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri)
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+db = client['my_database']
+accounts = db['account']
+users = db['user']
 
-class Image(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    path = db.Column(db.String(120), nullable=False)
-    
 @app.route("/")
 def home():
     if "username" in session:
         username = session["username"]
     else:
         username = 'Guest'
-    return render_template("index.html", username = username)
+    return render_template("index.html", username=username)
 
 @app.route('/Login', methods=['GET', 'POST'])
 def Login():
@@ -43,44 +42,46 @@ def Login():
         username = request.form["username"]
         password = request.form["password"]
         session["username"] = username
-        user = User.query.filter_by(username = username).first()
-        password = User.query.filter_by(password = password).first()
-        if user and password:
+
+        # Tìm người dùng trong MongoDB
+        user = accounts.find_one({'username': username, 'password': password})
+        if user:
             flash('Đăng nhập thành công!')
             return redirect('/')
         else:
-            flash('Tên đăng nhập hoặc mật khẩu không đúng')
+            return render_template('Login.html',message = 'Tên đăng nhập hoặc mật khẩu không đúng')
     return render_template('Login.html')
 
-@app.route('/SignUp', methods=['GET', 'POST'])
+@app.route('/Signup', methods=['GET', 'POST'])
 def Signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template('SignUp.html', message='Đăng ký thành công!')
-        # return redirect('/Login')
-    return render_template('SignUp.html')
+        # Thêm người dùng mới vào MongoDB
+        if accounts.find_one({'username': username}):
+            return render_template('Signup.html', message='Tài khoản đã tồn tại')
+        else:
+            new_user = {'username': username, 'password': password}
+            accounts.insert_one(new_user)
+            return render_template('Signup.html', message='Đăng ký thành công!')
+    return render_template('Signup.html')
 
 @app.route("/Logout")
 def logout():
-    session.pop("username",None)
+    session.pop("username", None)
     return render_template('Login.html', message='Đăng xuất thành công!')
 
 @app.route('/Upload/<username>', methods=['GET', 'POST'])
 def Upload_file(username):
     if "username" not in session:
-        return render_template('Login.html', message = "Bạn chưa đăng nhập")
+        return render_template('Login.html', message="Bạn chưa đăng nhập")
     else:
         imageCount = 0
         username = session["username"]
-        label_folder = os.path.join(app.config['UPLOAD_FOLDER'], username, 'Label')
-        if not os.path.exists(label_folder):
-            os.makedirs(label_folder)
-        for root, dirs, files in os.walk(label_folder):
-            imageCount += len(files)
+        image_folder =  os.path.join(app.config['UPLOAD_FOLDER'], username)
+        if not os.path.exists(image_folder):
+            os.makedirs(image_folder)
+        imageCount = users.count_documents({'user' : username, 'Type' : 'Label'})
         if "username" in session:
             if request.method == 'POST':
                 if 'file' not in request.files:
@@ -90,34 +91,38 @@ def Upload_file(username):
                 if file.filename == '':
                     print('No selected file')
                     return 'No selected file'
-                # Lưu file vào thư mục đã định
                 if file and allowed_file(file.filename):
                     filename = secure_filename(file.filename)
-                    print(imageCount)
-                    if imageCount < 5:
-                        file.save(os.path.join(label_folder, filename))
-                    train_folder = os.path.join(app.config['UPLOAD_FOLDER'], username, 'Train')
-                    if not os.path.exists(train_folder):
-                        os.makedirs(train_folder)
-                    file.save(os.path.join(train_folder, filename))
-                return render_template('Upload.html',username = username, message = "Upload thành công")
-    return render_template('Upload.html',username = username)
+                    # print(imageCount)
+                    if int(imageCount) < 5:
+                        print(imageCount)
+                        Label_image = {'user': username, 'Type': 'Label','Image_name':filename, 'path': os.path.join(image_folder, filename)}
+                        users.insert_one(Label_image)
+                    # Lưu thông tin ảnh vào MongoDB
+                    Train_image = {'user': username, 'Type': 'Train','Image_name':filename, 'path': os.path.join(image_folder, filename)}
+                    users.insert_one(Train_image)
+                    file.save(os.path.join(image_folder, filename))
+                return render_template('Upload.html', username=username, message="Upload thành công")
+    return render_template('Upload.html', username=username)
+
 @app.route('/ShowImage/<username>')
 def show_images(username):
     if "username" not in session:
-        return render_template("Login.html",username = username, message = 'Bạn chưa đăng nhập')
+        return render_template("Login.html", username=username, message='Bạn chưa đăng nhập')
     else:
-        user_folder = os.path.join(app.config['UPLOAD_FOLDER'], username,'Label')
-        imageConut = 0
-        for root, dirs, files in os.walk(user_folder):
-            imageConut += len(files)
-        if imageConut == 0 or not os.path.exists(user_folder):
-            render_template('ShowImage.html', username = username, message = "Bạn chưa upload ảnh")
+        imageCount = users.count_documents({'user' : username, 'Type' : 'Label'})
+        if imageCount == 0:
+            return render_template('ShowImage.html', username=username, message="Bạn chưa upload ảnh")
         else:
-            images = os.listdir(user_folder)
-            return render_template('ShowImage.html', username = username, images = images)
-    return render_template('ShowImage.html', username = username, message = "Bạn chưa upload ảnh")
-    
+            images_list = users.find({'user' : username, 'Type' : 'Label'})
+            images = []
+            for image_doc in images_list:
+                image_path = image_doc['path']
+                print(image_path)
+                image_name = image_path.split('\\').pop()
+                images.append(image_name)
+            return render_template('ShowImage.html', username=username, images=images)
+
 @app.route('/About')
 def About():
     return render_template("About.html")
@@ -125,49 +130,37 @@ def About():
 @app.route('/Save/<filename>')
 def Save(filename):
     username = session["username"]
-    path = "image_user/" + username + '/'
+    path = "Image/" + username + '/'
     return send_file(path + filename, as_attachment=True)
 
 @app.route('/draw/<image_name>')
 def draw(image_name):
     if 'username' in session:
         username = session['username']
-        image_path = f'/static/image_user/{username}/Label/{image_name}'
+        image_path = f'/static/Image/{username}/{image_name}'
         return render_template('DrawImage.html', username=username, image_path=image_path)
     return redirect(url_for('Login'))
-
 
 @app.route('/save-coordinates', methods=['POST'])
 def save_coordinates():
     username = session['username']
-    data = request.get_json()  # Get the JSON data from the request
+    data = request.get_json()
     image_name = data.get('imageName')
     coordinates = data.get('coordinates')
     if not image_name or not coordinates:
         return jsonify({'error': 'Invalid data'}), 400
-    # Create a filename based on the image name
-    directory = os.path.join("Flask", "image_user_rectangle", username)
+
+    directory = os.path.join("Image",username)
     if not os.path.exists(directory):
         os.makedirs(directory)
     filename = os.path.join(directory, f"{image_name}.txt")
-    # Prepare the text content to save
+
     with open(filename, 'w') as f:
         for i, coord in enumerate(coordinates):
             f.write(f"{coord['x1']},{coord['y1']},{coord['x2']},{coord['y2']}\n")
+    Coord_image = {'user': username, 'Type': 'Coordinate','Image_name':f"{image_name}.txt", 'path': filename}
+    users.insert_one(Coord_image)
     return jsonify({'message': 'Coordinates saved successfully!'}), 200
-def ve():
-    with open('Flask/image_user_rectangle/chau/appli1.txt', 'r') as f:
-        coords = f.readline().split(',')
-        x1, y1, x2, y2 = map(float, coords)
-    img = cv2.imread('Flask/image_user/chau/appli1.jpg')
-    # Vẽ hình chữ nhật
-    cv2.rectangle(img, (int(x1),int(y1)), (int(x2),int(y2)), (0,0,255), 1)
-    # Hiển thị ảnh
-    cv2.imshow("img",img) # Use cv2_imshow instead of cv2.imshow
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-with app.app_context():
-    db.create_all()
-if __name__=="__main__":
-    app.run(debug= True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Flask/static/script.js
+++ Flask/static/script.js
@@ -80,4 +80,3 @@ window.onload = function() {
           });
     });
 }
-
